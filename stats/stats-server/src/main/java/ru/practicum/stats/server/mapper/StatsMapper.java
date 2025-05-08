c4276ec4c82ddbadd5647ea3542c500ef9bce9ac
Gomer <alekseev.m.a.rus@gmail.com>
2025-05-08 21:57:40 +0300

    add-stats-server-repository_2

--- stats/stats-server/src/main/java/ru/practicum/stats/server/mapper/StatsMapper.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/mapper/StatsMapper.java
@@ -1,13 +1,17 @@
 package ru.practicum.stats.server.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.stats.dto.EndpointHitDto;
 import ru.practicum.stats.server.model.EndpointHit;
 
 @Mapper(componentModel = "spring")
 public interface StatsMapper {
+
+    @Mapping(target = "timestamp", expression = "java(source.getTimestamp().toLocalDateTime())")
     EndpointHit mapToModel(EndpointHitDto endpointHitDto);
 
+    @Mapping(target = "timestamp", expression = "java(java.sql.Timestamp.valueOf(source.getTimestamp()))")
     EndpointHitDto mapToDto(EndpointHit endpointHit);
 
 }
