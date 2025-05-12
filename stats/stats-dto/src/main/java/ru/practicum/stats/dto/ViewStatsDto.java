d79c4d56966b621be37ae2cee502fbd477e579f7
Gomer <alekseev.m.a.rus@gmail.com>
2025-05-12 15:47:21 +0300

    Исправить замечания

--- stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats/dto/ViewStatsDto.java
@@ -1,6 +1,10 @@
 package ru.practicum.stats.dto;
 
-import lombok.*;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
