fa34ef91dff8e9e3799c5f5929a69182f81efb5a
Evgeniy Kats <eSunr1se@yandex.ru>
2025-05-17 02:05:16 +0500

    Добавить контроллер запросы

---
+++ main-service/src/main/java/ru/practicum/main/service/request/controller/PrivateRequestController.java
@@ -0,0 +1,52 @@
+package ru.practicum.main.service.request.controller;
+
+import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.main.service.request.dto.ParticipationRequestDto;
+import ru.practicum.main.service.request.service.RequestService;
+
+import java.util.List;
+
+@Validated
+@Slf4j
+@RestController
+@RequestMapping("/users/{userId}/requests")
+@RequiredArgsConstructor
+public class PrivateRequestController {
+    private final RequestService requestService;
+
+    @GetMapping
+    public List<ParticipationRequestDto> getParticipationRequests(@Min(1) @PathVariable Long userId) {
+        log.info("Получен GET /users/{}/requests", userId);
+        List<ParticipationRequestDto> participationRequests = requestService.getParticipationRequests(userId);
+        log.info("Найдено {} заявок от пользователя {}", participationRequests.size(), userId);
+        return participationRequests;
+    }
+
+    @PostMapping
+    public ParticipationRequestDto createParticipationRequest(@Min(1) @PathVariable Long userId,
+                                                              @Min(1) @RequestParam Long eventId) {
+        log.info("Получен POST /users/{}/requests , eventId = {}", userId, eventId);
+        ParticipationRequestDto participationRequest = requestService.createParticipationRequest(userId, eventId);
+        log.info("Успешно создана заявка пользователем = {} на участие в мероприятити = {}", userId, eventId);
+        return participationRequest;
+    }
+
+    @PatchMapping("/{requestId}/cancel")
+    public ParticipationRequestDto updateParticipationRequest(@Min(1) @PathVariable Long userId,
+                                                              @Min(1) @PathVariable Long requestId) {
+        log.info("Получен PATCH /users/{}/requests , eventId = {}", userId, requestId);
+        ParticipationRequestDto participationRequest = requestService.updateParticipationRequest(userId, requestId);
+        log.info("Успешно отменена заявка пользователем = {} на участие в мероприятити = {}", userId, requestId);
+        return participationRequest;
+    }
+}
