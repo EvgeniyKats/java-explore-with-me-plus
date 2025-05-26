e2a98e8b4a875ac348ec241f3a3a066abca59a7d
desidiam <desidiam@gmail.com>
2025-05-26 19:04:12 +0300

    Добавлена валидация на переменные в контроллеры, изменена аннотация с RestController на Controller

--- main-service/src/main/java/ru/practicum/main/service/comment/controller/PublicCommentController.java
+++ main-service/src/main/java/ru/practicum/main/service/comment/controller/PublicCommentController.java
@@ -1,25 +1,30 @@
 package ru.practicum.main.service.comment.controller;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.practicum.main.service.comment.dto.GetCommentDto;
 import ru.practicum.main.service.comment.enums.CommentSortType;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/events/{eventId}/comments")
 @Slf4j
 public class PublicCommentController {
 
     @GetMapping("/{id}")
-    public ResponseEntity<GetCommentDto> getComment(@PathVariable final String eventId,
-                                                    @PathVariable final String id) {
+    public ResponseEntity<GetCommentDto> getComment(@PathVariable @Positive final String eventId,
+                                                    @PathVariable @Positive final String id) {
         log.info("Получение комментария по id и eventId : {}, {}", id, eventId);
         GetCommentDto comment = new GetCommentDto();
         log.info("Отдан комментарий с телом {}", comment);
@@ -28,7 +33,7 @@ public class PublicCommentController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GetCommentDto>> getComments(@PathVariable final String eventId,
+    public ResponseEntity<List<GetCommentDto>> getComments(@PathVariable @Positive final String eventId,
                                                            @RequestParam(value = "from", required = false, defaultValue = "0") @Min(0) Integer from,
                                                            @RequestParam(value = "size", required = false, defaultValue = "10") @Min(1) Integer size,
                                                            @RequestParam(value = "sort", required = false, defaultValue = "COMMENTS_NEW")
