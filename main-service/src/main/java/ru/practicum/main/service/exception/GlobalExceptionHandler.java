a7b960962f3c4a7eb5cff4f53d94e8477f2375d1
Ivan Korolkov <nerounxqxq@gmail.com>
2025-05-27 13:31:57 +0400

    Добавлена проверка автора комментария, проверка id мероприятия

--- main-service/src/main/java/ru/practicum/main/service/exception/GlobalExceptionHandler.java
+++ main-service/src/main/java/ru/practicum/main/service/exception/GlobalExceptionHandler.java
@@ -31,4 +31,10 @@ public class GlobalExceptionHandler {
     public ApiError handleConflict(final ConflictException e) {
         return e.getError();
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ApiError handleForbidden(final ForbiddenException e) {
+        return e.getError();
+    }
 }
