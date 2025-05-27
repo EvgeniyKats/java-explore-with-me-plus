31b944536362f9e44150606607b1129b92d5907b
Ivan Korolkov <nerounxqxq@gmail.com>
2025-05-27 23:49:38 +0400

    Удалена проверка на присутствие сущностей в БД

--- main-service/src/main/java/ru/practicum/main/service/Constants.java
+++ main-service/src/main/java/ru/practicum/main/service/Constants.java
@@ -9,7 +9,7 @@ public interface Constants {
     String EVENT_NOT_FOUND = "Событие не найдено";
     String CATEGORY_NOT_FOUND = "Категория не найдена";
     String COMMENT_NOT_FOUND = "Комментарий не найден";
-    String USER_NOT_THE_AUTHOR = "Пользователь не является автором комментария";
     String COMMENT_EVENT_NOT_MATCH = "Мероприятие к которому был написан комментарий не совпадает с указанным в запросе";
+    String COMMENT_AUTHOR_NOT_MATCH = "Автор комментария не совпадает с пользователем, указанным в запросе";
     LocalDateTime MIN_START_DATE = LocalDateTime.of(1970, 1, 1, 0, 0);
 }
