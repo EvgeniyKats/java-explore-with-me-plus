d4ac2a7659955cf9612fc80e49a2bb2aa89a406b
desidiam <desidiam@gmail.com>
2025-05-27 19:02:47 +0300

    Исправлены типы в id, убраны значения у аннотаций, добавлена аннотация validated, изменен путь контроллера getComment.

--- main-service/src/main/java/ru/practicum/main/service/comment/controller/PublicCommentController.java
+++ main-service/src/main/java/ru/practicum/main/service/comment/controller/PublicCommentController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +21,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/events/{eventId}/comments")
 @Slf4j
+@Validated
 public class PublicCommentController {
 
-    @GetMapping("/{id}")
-    public ResponseEntity<GetCommentDto> getComment(@PathVariable @Positive final String eventId,
-                                                    @PathVariable @Positive final String id) {
-        log.info("Получение комментария по id и eventId : {}, {}", id, eventId);
+    @GetMapping("/{commentId}")
+    public ResponseEntity<GetCommentDto> getComment(@PathVariable @Positive final Long eventId,
+                                                    @PathVariable @Positive final Long commentId) {
+        log.info("Получение комментария по id и eventId : {}, {}", commentId, eventId);
         GetCommentDto comment = new GetCommentDto();
         log.info("Отдан комментарий с телом {}", comment);
         return ResponseEntity.ok(comment);
@@ -33,11 +35,10 @@ public class PublicCommentController {
     }
 
     @GetMapping
-    public ResponseEntity<List<GetCommentDto>> getComments(@PathVariable @Positive final String eventId,
-                                                           @RequestParam(value = "from", required = false, defaultValue = "0") @Min(0) Integer from,
-                                                           @RequestParam(value = "size", required = false, defaultValue = "10") @Min(1) Integer size,
-                                                           @RequestParam(value = "sort", required = false, defaultValue = "COMMENTS_NEW")
-                                                           CommentSortType sort) {
+    public ResponseEntity<List<GetCommentDto>> getComments(@PathVariable @Positive final Long eventId,
+                                                           @RequestParam(defaultValue = "0") @Min(0) Integer from,
+                                                           @RequestParam(defaultValue = "10") @Min(1) Integer size,
+                                                           @RequestParam(defaultValue = "COMMENTS_NEW") CommentSortType sort) {
         log.info("Получение комментариев на мероприятие {}", eventId);
         List<GetCommentDto> comments = new ArrayList<>();
         log.info("Сформирован ответ с телом: {}", comments);
